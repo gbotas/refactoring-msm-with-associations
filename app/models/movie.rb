fa1a5483aeebe20b844ee605528a5c2e10e06c73
gbotas <gbotas@uchicago.edu>
2025-05-08 17:17:30 +0000

    final commit

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -19,4 +19,5 @@ class Movie < ApplicationRecord
   belongs_to(:director, class_name: "Director", foreign_key: "director_id", primary_key: "id")
 
   has_many(:characters, class_name: "Character", foreign_key: "movie_id")
+  has_many(:cast, through: :characters, source: :actor)
 end
